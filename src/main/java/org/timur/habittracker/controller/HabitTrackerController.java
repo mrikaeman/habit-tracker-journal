d13d2d976d73c6cb9871d0f32a12fd28a55e587e
novak <novaktimur@gmail.com>
2026-03-21 18:44:57 +0100

    Addded ability to add highlight per day

--- src/main/java/org/timur/habittracker/controller/HabitTrackerController.java
+++ src/main/java/org/timur/habittracker/controller/HabitTrackerController.java
@@ -2,9 +2,12 @@ package org.timur.habittracker.controller;
 
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.timur.habittracker.service.HabitTrackerService;
 
+import java.time.LocalDate;
 import java.time.YearMonth;
 
 @Controller
@@ -22,5 +25,12 @@ public class HabitTrackerController {
         model.addAttribute("monthRows", habitTrackerService.buildCurrentMonthView());
         model.addAttribute("currentMonth", YearMonth.now());
         return "tracker";
+    }
+
+    @PostMapping("/highlight")
+    public String updateHighlight(@RequestParam LocalDate date,
+                                  @RequestParam String dailyHighlight) {
+        habitTrackerService.updateDailyHighlight(date, dailyHighlight);
+        return "redirect:/";
     }
 }
